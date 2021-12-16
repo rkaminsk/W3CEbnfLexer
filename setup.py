90c0d0b0fd709f3829722b9557e0fb0afd37cf1e
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2021-12-16 19:59:35 +0100

    update setup.py to upload on pypi

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='w3c_ebnflexer',
+    name='pygments_w3c_ebnf',
+    version='0.1',
     packages=find_packages(),
+    description='A pygments lexer for the Extended Backus-Naur Format used by the W3C',
+    url='https://github.com/rkaminsk/W3CEbnfLexer',
+    author='William Heinbockel, Roland Kaminski',
+    license='Apache 2.0',
     entry_points="""
 [pygments.lexers]
 w3c_ebnflexer = w3c_ebnflexer:W3CEbnfLexer
